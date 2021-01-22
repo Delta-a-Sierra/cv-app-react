90f2f5eaa0af3866e66857741c97df98b69dfc98
Dwayne Sutherland <dwayneasutherland@gmail.com>
2021-01-22 00:53:25 +0000

    refactored Job Component to use Hooks instead of Class state

--- src/components/professionalHistory/Job.js
+++ src/components/professionalHistory/Job.js
@@ -1,59 +1,43 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 import uuid from 'react-uuid'
 import ListItem from '../../utils/listItem/ListItem'
 import './style.scss'
 import RegularButton from '../../utils/regularButton/RegularButton'
 
-class Job extends Component{
-    constructor(props){
-        super(props)
+const Job = (props) => {
 
-        this.state = {
-            descriptions : [{value: 'worked on things', key: uuid()}]
-        }
+    const [descriptions, setDescriptions] = useState([{value: 'worked on things', key: uuid()}])
 
-        this.addDescription = this.addDescription.bind(this)
-        this.deleteDescription = this.deleteDescription.bind(this)
+    const addDescription = () => {
+        setDescriptions([...descriptions, {value: 'Lorem Ipsum', key: uuid()}])
     }
 
-    addDescription(){
-        this.setState({
-            descriptions : [...this.state.descriptions, {value: 'Lorem Ipsum', key: uuid()}]
-        })
+    const deleteDescription = (key) => {
+        const newDescription = descriptions.filter((description) => description.key !== key)
+        setDescriptions(newDescription)
     }
 
-    deleteDescription(key){
-        console.log('Deleting Description')
-        const newDescription = this.state.descriptions.filter((description) => description.key !== key)
-        this.setState({
-            descriptions : newDescription
-        })
-    }
+    const {jobTitle, employerName, startDate, endDate} = this.props
 
-    render(){
-        const {jobTitle, employerName, startDate, endDate} = this.props
+    const descriptionList = descriptions.map((description) => <ListItem key={description.key} mode={props.mode}
+    value={description.value} onDelete={() => deleteDescription(description.key)}/>)
 
-        const descriptionList = this.state.descriptions.map((description) => <ListItem key={description.key} mode={this.props.mode}
-        value={description.value} onDelete={() => this.deleteDescription(description.key)}/>)
-
-        return(
-            <div className="Job">
-                <h4>{jobTitle}</h4>
-                <div className="JobDetails">
-                    <h5>{employerName}</h5>
-                    <h5>{startDate}-{endDate}</h5>
-                </div>
-                <ul>
-                    {descriptionList}
-                </ul>
-                <div className="btm-btns">
-                    <RegularButton onClick={this.addDescription} text='New Description'/>
-                    <RegularButton onClick={this.props.onDelete} text='Delete Job'/>
-                </div>
-
+    return(
+        <div className="Job">
+            <h4>{jobTitle}</h4>
+            <div className="JobDetails">
+                <h5>{employerName}</h5>
+                <h5>{startDate}-{endDate}</h5>
             </div>
-        )
-    }
+            <ul>
+                {descriptionList}
+            </ul>
+            <div className="btm-btns">
+                <RegularButton onClick={addDescription} text='New Description'/>
+                <RegularButton onClick={props.onDelete} text='Delete Job'/>
+            </div>
+        </div>
+    )
 }
 
 export default Job
