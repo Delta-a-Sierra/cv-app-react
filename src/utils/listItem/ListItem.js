b2772af8dc2ae7cf968fc7c1e1b904cf03c16bf0
Dwayne Sutherland <dwayneasutherland@gmail.com>
2021-01-22 00:06:19 +0000

    refactored ListItem Util Component to use Hooks instease of Class state

--- src/utils/listItem/ListItem.js
+++ src/utils/listItem/ListItem.js
@@ -1,39 +1,27 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 
-class ListItem extends Component{
-    constructor(props){
-        super(props)
+const ListItem = (props) => {
 
-        this.state = {
-            value : this.props.value
-        }
+    const [listValue, setListValue] = useState(props.value)
 
-        this.changeValue = this.changeValue.bind(this)
+    const changeValue = (e) => {
+        setListValue(e.target.value)
     }
 
-    changeValue(e){
-        this.setState({
-            value : e.target.value
-        })
-    }
-
-    render(){
-
-        const {key, value} = this.state
-        if(this.props.mode === 'preview'){
-            return(
-                <li key={key}>{value}</li>
-            )
-        }
+    const key = props.key
+    if(props.mode === 'preview'){
         return(
-            <div>
-                <input type="text" onChange={this.changeValue} value={value}></input>
-                <button className="input-del-btn" onClick={this.props.onDelete}>Delete</button>
-            </div>
-
+            <li key={key}>{listValue}</li>
         )
-
     }
+
+    return(
+        <div>
+            <input type="text" onChange={changeValue} value={listValue}></input>
+            <button className="input-del-btn" onClick={props.onDelete}>Delete</button>
+        </div>
+
+    )
 }
 
 export default ListItem
