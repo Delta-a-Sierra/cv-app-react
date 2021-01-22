a8355ffc4f24ddecc9f23beb89a4ec01e84fd863
Dwayne Sutherland <dwayneasutherland@gmail.com>
2021-01-22 00:32:36 +0000

    refactored Contact Component to use Hooks instead of Class state

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,78 +1,87 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 import './style.scss'
 import Title from '../../utils/title/Title'
 
-class Contact extends Component{
-    constructor(props){
-        super(props)
+const Contact = () => {
+    
+    const [mode, setMode] = useState('preview')
+    const [number, setNumber] = useState('123')
+    const [email, setEmail] = useState('email@email.com')
+    const [street, setStreet] = useState('1 someroad')
+    const [area, setArea] = useState('camberwell')
+    const [city, setCity] = useState('London')
+    const [postcode, setPostcode] = useState('NW1 1AB')
 
-        this.state = {
-            mode: 'preview',
-            number: '123',
-            email: "email@email.com",
-            street: "1 someroad",
-            area : "camberwell",
-            city: "London",
-            postcode : "NW1 1AB"
+
+    const changeMode = () => {
+        if(mode === 'edit'){
+            setMode('preview')
+            return null
         }
-
-        this.changeMode = this.changeMode.bind(this)
+        setMode('edit')
     }
 
-    changeMode(){
-        if(this.state.mode === 'edit'){
-            this.setState({
-                mode : 'preview'
-            })
-            return
-        }
-        this.setState({
-            mode : 'edit'
-        })
+    const changeNumber = (e) => {
+        setNumber(e.target.value)
     }
 
-    render(){
-        const {number, email, street, area, city, postcode, mode} = this.state
-        if(mode === 'preview'){
-            return(
-                <div className="Contact">
-                    <Title mode={mode} changeMode={this.changeMode} titleText="Contact"/>   
-                    <p>{number}</p>
-                    <p>{email}</p>
-                    <p>{street}</p>
-                    <p>{area}</p>
-                    <p>{city}</p>
-                    <p>{postcode}</p>
-                </div>
-            )
-        }
+    const changeEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const changeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+
+    const changeArea = (e) => {
+        setArea(e.target.value)
+    }
+
+    const changeCity = (e) => {
+        setCity(e.target.value)
+    }
+
+    const changePostcode = (e) => {
+        setPostcode(e.target.value)
+    }
+
+
+    if(mode === 'preview'){
         return(
             <div className="Contact">
-                <Title mode={mode} changeMode={this.changeMode} titleText="Contact"/>
-                <label> Number
-                <input type="text" value={number}></input>
-                </label>
-                <label> email
-                <input type="text" value={email}></input>
-                </label>
-                <label> street
-                <input type="text" value={street}></input>
-                </label>
-                <label> area
-                <input type="text" value={area}></input>
-                </label>
-                <label> city
-                <input type="text" value={city}></input>
-                </label>
-                <label> postcode
-                <input type="text" value={postcode}></input>
-                </label>
+                <Title mode={mode} changeMode={changeMode} titleText="Contact"/>   
+                <p>{number}</p>
+                <p>{email}</p>
+                <p>{street}</p>
+                <p>{area}</p>
+                <p>{city}</p>
+                <p>{postcode}</p>
             </div>
         )
     }
-
-
-
+    return(
+        <div className="Contact">
+            <Title mode={mode} changeMode={changeMode} titleText="Contact"/>
+            <label> Number
+            <input onChange={changeNumber} type="text" value={number}></input>
+            </label>
+            <label> email
+            <input onChange={changeEmail}  type="text" value={email}></input>
+            </label>
+            <label> street
+            <input onChange={changeStreet} type="text" value={street}></input>
+            </label>
+            <label> area
+            <input onChange={changeArea} type="text" value={area}></input>
+            </label>
+            <label> city
+            <input onChange={changeCity} type="text" value={city}></input>
+            </label>
+            <label> postcode
+            <input onChange={changePostcode} type="text" value={postcode}></input>
+            </label>
+        </div>
+    )
 }
 
 export default Contact
