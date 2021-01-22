a8fa13fdcec1202deea2b1b1c15f0596f66544c8
Dwayne Sutherland <dwayneasutherland@gmail.com>
2021-01-22 00:38:25 +0000

    refactored Header Component to use Hooks instead of Class state

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,68 +1,50 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 import './style.scss'
 import ModeToggleBtn from '../../utils/modeToggleBtn/ModeToggleBtn'
 
-class Header extends Component{
-    constructor(props){
-        super(props)
+const Header = () => {
+    this.state = {
+        mode : 'preview',
+        name : 'Firstname Surname',
+        jobTitle: 'Job Title'
+    }
 
-        this.state = {
-            mode : 'preview',
-            name : 'Firstname Surname',
-            jobTitle: 'Job Title'
+    const [mode, setMode] = useState('preview')
+    const [name, setName] = useState('Firstname Surname')
+    const [jobTitle, setJobTitle] = useState('Job Title')
+
+    const changeMode = () => {
+        if(mode === 'edit'){
+            setMode('preview')
+            return null
         }
-
-        this.changeName = this.changeName.bind(this)
-        this.changeTitle = this.changeTitle.bind(this)
-        this.changeMode = this.changeMode.bind(this)
+        setMode('edit')
     }
 
-    changeName(e){
-        this.setState({
-            name : e.target.value
-        })
+    const changeName = (e) => {
+        setName(e.target.value)
     }
 
-    changeTitle(e){
-        this.setState({
-            jobTitle : e.target.value
-        })
+    const changeTitle = (e) => {
+        setJobTitle(e.target.value)
     }
 
-
-    changeMode(){
-        if(this.state.mode === 'edit'){
-            this.setState({
-                mode : 'preview'
-            })
-            return
-        }
-        this.setState({
-            mode : 'edit'
-        })
-
-    }
-
-    render(){
-
-        const {mode, name, jobTitle} = this.state
-        if(mode === 'preview'){
-            return(
-                <div className="Title">
-                    <h1 className="name">{name}</h1>
-                    <ModeToggleBtn mode={mode} changeMode={this.changeMode} />
-                    <h1>{jobTitle}</h1>
-                </div>
-            )
-        }
+    if(mode === 'preview'){
         return(
             <div className="Title">
-                <input className="name" type="text" value={name} onChange={this.changeName}></input>
+                <h1 className="name">{name}</h1>
                 <ModeToggleBtn mode={mode} changeMode={this.changeMode} />
-                <input type="text" value={jobTitle} onChange={this.changeTitle}></input>
+                <h1>{jobTitle}</h1>
             </div>
         )
     }
+    return(
+        <div className="Title">
+            <input className="name" type="text" value={name} onChange={changeName}></input>
+            <ModeToggleBtn mode={mode} changeMode={changeMode} />
+            <input type="text" value={jobTitle} onChange={changeTitle}></input>
+        </div>
+    )
 }
 
 export default Header
